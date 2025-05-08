799b6a5fdf849d702ba8cf7f3eacaf795ca47b76
gololobov <gololobov.serhiy@gmail.com>
2025-05-08 16:05:13 +0300

    06.05.2025

---
+++ Lesson 3.py
@@ -0,0 +1,41 @@
+def printLine(count, symbol):
+    print(symbol * count)
+
+# printLine(20, '*')
+#
+# print("mama")
+#
+# printLine(30, '$')
+#
+# print("Hello")
+#
+# printLine(40, '#')
+# printLine(10, 'mama ')
+
+def summa(a, b):
+    c = a + b
+    return c
+
+# res = summa(23, 3)
+# print(summa(3, 5))
+
+
+def square(n):
+    printLine("* ", n)
+    for i in range(n-2):
+        print("* ", end='')
+        for j in range(n-2):
+            print("  ", end='')
+        print("* ", end='')
+        print()
+    printLine("* ", n)
+
+
+
+square(4)
+
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * *
