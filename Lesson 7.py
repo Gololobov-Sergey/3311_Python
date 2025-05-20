25808358f1f35f9dc434addd18dddc9c5a0db2f3
gololobov <gololobov.serhiy@gmail.com>
2025-05-20 17:46:35 +0300

    20.05.2025

---
+++ Lesson 7.py
@@ -0,0 +1,48 @@
+import os
+
+path = "C:\\Users\\gololobov\\Downloads\\Logo\\Python.jpg"
+path = os.path.normpath(path)
+
+# for path, dirnames, filenames in os.walk(path):
+#     print(path)
+#     print(dirnames)
+#     print(filenames)
+
+# dir = "Test"
+# path = os.path.join(path, dir)
+# os.mkdir(path)
+
+# print(os.path.isdir(path))
+# print(os.path.isfile(path))
+# print(os.path.isabs(path))
+# print(os.path.exists(path))
+#
+#
+# print(os.path.getctime(path))
+# print(os.path.getmtime(path))
+# print(os.path.getatime(path))
+# print(os.path.getsize(path))
+
+
+# f = open("test.txt", "w")
+# f.write(str(555))
+# f.close()
+
+# l = [23,34,45,5,67]
+# print(sum(l))
+
+size = 10
+l = []
+
+
+
+# f = open("test.txt", "r")
+# st = f.read().split()
+# print(st)
+# for i in range(len(st)):
+#     st[i] = int(st[i])
+# print(st)
+# print(sum(st))
+
+
+
