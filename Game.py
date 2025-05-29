f3d657b20c19ff5d840373f4b4b2ea9c278bd3ad
gololobov <gololobov.serhiy@gmail.com>
2025-05-29 17:38:37 +0300

    29.05.2025

--- Game.py
+++ Game.py
@@ -29,12 +29,66 @@ label_bomb = tk.Label(image=img1)
 label_bomb.pack()
 
 
+def is_alive():
+    global time
+    if time <= 0:
+        time = 0
+        label.config(text='Ба-бах!', fg='red')
+        return False
+    else:
+        return True
+
+def update_screen():
+    if time >= 80:
+        label_bomb.config(image=img1)
+    elif 80 > time >= 50:
+        label_bomb.config(image=img2)
+    elif 0 < time < 50:
+        label_bomb.config(image=img3)
+    else:
+        label_bomb.config(image=img4)
+
+    label_time.config(text=f'Залишок часу {time}')
+    label_score.config(text=f'Набрані бали {score}')
+    label_bomb.after(100, update_screen)
+
+
+
+def update_time():
+    global time
+    time -= 5
+    if is_alive():
+        label_time.after(500, update_time)
+
+
+def update_score():
+    global score
+    score += 1
+    if is_alive():
+        label_score.after(1000, update_score)
+
+
+def start(event):
+    global press_enter
+    if not press_enter:
+        pass
+    else:
+        press_enter = False
+        label.config(text='')
+        update_score()
+        update_time()
+        update_screen()
+
 def click():
-    pass
+    global time
+    if is_alive() and press_enter == False:
+        time += 1
 
 
 button = tk.Button(text='Тикай сюди', font=('Comic Sans MS', 16, 'bold'), bg='black', fg='white', command=click)
 button.pack()
+
+window.bind("<Return>", start)
 
 
 window.mainloop()
